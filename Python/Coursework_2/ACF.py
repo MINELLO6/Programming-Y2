39d3fcb243e8551f0a7dad95089d86d673c0b954
MINELLO6 <92674808+MINELLO6@users.noreply.github.com>
2023-03-20 19:19:58 +0800

    Update ACF.py

--- Python/Coursework_2/ACF.py
+++ Python/Coursework_2/ACF.py
@@ -28,8 +28,6 @@
 # #
 # #     return x1, estimated_relative_error(x0, x1), k
 #
-# def my_function(x):
-#     return (x**3-2*x**2+2*x)/(2*x**2+1)
 #
 # def secant_method_list(f,x0,x1,tol,kmax):
 #     if tol <= 0:
@@ -54,22 +52,30 @@
 #             break
 #     return my_list
 #
-# def my_function(x):
-#     return (x**3-2*x**2+2*x)/(2*x**2+1)
-#
 # import numpy as np
 # import matplotlib.pyplot as plt
-#
-# fig = plt.figure(figsize=(20, 10))
+# fig = plt.figure(figsize=(20,10))
 # ax = fig.add_subplot(111)
-# x = np.linspace(-3,3)
+# x=np.linspace(-3,5)
 # f_x = (x**3-2*x**2+2*x)/(2*x**2+1)
 # line_f_x = ax.plot(x, f_x)
 # points_x=[-1, 1, 0.6666666666666667, 2.8888888888888884, -1.7968024755028384]
 # points_y=[-1.6666666666666667, 0.3333333333333333, 0.39215686274509803, 0.7459099015274866, -2.1257318138669765]
 # ax.scatter(points_x, points_y, color='r', s=100)
-# ax.plot(points_x, points_y, linewidth=2.5)
-# ax.set_xticks(points_x)
+# ax.scatter(points_x, [0]*5, color='k', s=100, marker='x')
+# for i in range(len(points_x)-2):
+#     k = (points_y[i]-points_y[i+1])/(points_x[i]-points_x[i+1])
+#     b = points_y[i]-k*points_x[i]
+#     my_line = k*x+b
+#     # print(my_line)
+#     ax.plot(x,my_line, 'g')
+# # ax.plot(points_x, points_y)
+# for i in range(len(points_x)):
+#     ax.annotate(f'point {i}', xy=(points_x[i],points_y[i]), xytext=(points_x[i],points_y[i]+0.5),textcoords='data',
+#     arrowprops=dict(arrowstyle="->", connectionstyle="arc3"))
+# ax.axhline(y=0,color='k')
+# ax.set_ylim(-3,3)
+# # ax.set_xticks(points_x)
 # ax.title.set_text('Secant Method')
 # ax.set_xlabel('$x$')
 # ax.set_ylabel('$y$')
